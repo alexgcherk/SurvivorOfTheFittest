43b77cc053897bef156852980a6f9a773af3ba4c
Administrator <musor@xpiea.com>
2020-06-27 20:12:26 -0400

    First commit

---
+++ Assets/ServerAPI/ServerTypes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace CommonServerAPI 
+{
+    public interface IServer
+    {
+        string Ping();
+        Response Execute(Request request);
+
+    }
+
+    public abstract class Request
+    {
+
+    }
+
+    public abstract class Response
+    {
+
+    }
+}
